53536bf3aebd9ae0fe5b38736da54028e2176574
arungandhi0503 <61384255+arungandhi0503@users.noreply.github.com>
2024-09-15 02:25:28 +0200

    feature: Custom MUI react components

--- src/index.js
+++ src/index.js
@@ -3,15 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Solutions from './components/Solutions';
+import ErrorPage from './components/ErrorPage';
+import Services from './components/Services';
+import Products from './components/Products';
+import Dashboard from './components/Dashboard';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />
+      },
+      {
+        path: "/solutions",
+        element: <Solutions />
+      },
+      {
+        path: "/services",
+        element: <Services />
+      },
+      {
+        path: "/products",
+        element: <Products />
+      },
+      {
+        path: "*",
+        element: <ErrorPage />
+      }]
+  }]
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
