d7a300940078dfd56581fd35dafb32cc04e8a90d
arungandhi0503 <61384255+arungandhi0503@users.noreply.github.com>
2024-09-16 09:36:00 +0200

    fix: Style modified and removed commented code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-// import MuiCard from '@mui/material/Card';
 import Checkbox from '@mui/material/Checkbox';
 import Divider from '@mui/material/Divider';
 import FormLabel from '@mui/material/FormLabel';
@@ -12,30 +11,11 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import ForgotPassword from './ForgetPassword';
 import Grid from '@mui/material/Grid2';
-// import Paper from '@mui/material/Paper';
 import { ThemeProvider, createTheme } from '@mui/material';
-// import { styled } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
 
 import { GoogleIcon, FacebookIcon } from './CustomIcons';
 
-// const Card = styled(MuiCard)(({ theme }) => ({
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignSelf: 'center',
-//     width: '100%',
-//     padding: theme.spacing(4),
-//     gap: theme.spacing(2),
-//     boxShadow:
-//         'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-//     [theme.breakpoints.up('sm')]: {
-//         width: '450px',
-//     },
-//     ...theme.applyStyles('dark', {
-//         boxShadow:
-//             'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
-//     }),
-// }));
 const primaryButtonTheme = createTheme({
     components: {
         MuiButton: {
