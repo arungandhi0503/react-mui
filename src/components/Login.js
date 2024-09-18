c821d8243c2b5779adf43e616eefc699a22ec69a
arungandhi0503 <61384255+arungandhi0503@users.noreply.github.com>
2024-09-18 11:26:30 +0200

    fix: CSS correction

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,9 +13,8 @@ import ForgotPassword from './ForgetPassword';
 import Grid from '@mui/material/Grid2';
 import { ThemeProvider, createTheme } from '@mui/material';
 import Stack from '@mui/material/Stack';
-
-import { GoogleIcon, FacebookIcon } from './CustomIcons';
-
+import AppleIcon from '@mui/icons-material/Apple';
+import GoogleIcon from '@mui/icons-material/Google';
 const primaryButtonTheme = createTheme({
     components: {
         MuiButton: {
@@ -48,34 +47,6 @@ const secondaryButtonTheme = createTheme({
                     borderRadius: '30px',
                     '&:hover': {
                         backgroundColor: '#fff',
-                    },
-                },
-            },
-        },
-    },
-});
-const textTheme = createTheme({
-    components: {
-        MuiInput: {
-            styleOverrides: {
-                root: {
-                    '& .MuiOutlinedInput-root': {
-                        '& fieldset': {
-                            borderColor: '#666',
-                            borderRadius: '30px'
-                        },
-                        '&:hover fieldset': {
-                            borderColor: '#666',
-                        },
-                        '&.Mui-focused fieldset': {
-                            borderColor: '#666',
-                        },
-                    },
-                    '& .MuiInputLabel-root': {
-                        color: '#000',
-                    },
-                    '& .MuiInputLabel-root.Mui-focused': {
-                        color: '#333',
                     },
                 },
             },
@@ -145,177 +116,163 @@ function Login() {
     return (
         <>
 
-            <Stack
-                direction="column"
-                component="main"
-                sx={[
-                    {
-                        justifyContent: 'space-between',
-                        height: { xs: 'auto', md: '100%' },
-                    },
-                    (theme) => ({
-                        backgroundImage:
-                            'radial-gradient(ellipse at 70% 51%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-                        backgroundSize: 'cover',
-                        ...theme.applyStyles('dark', {
-                            backgroundImage:
-                                'radial-gradient(at 70% 51%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-                        }),
-                    }),
-                ]}
-            >
-                <Grid container spacing={4} sx={{
-                    justifyContent: 'center',
-                    p: 5,
-                    m: 'auto',
-                }}>
-                    <Grid spacing={4} size={{ xs: 12, md: 6 }} className="bg-img"
-                        sx={{ flexDirection: 'column', alignSelf: 'auto', gap: 4, color: '#fff', height: '65vh', padding: 6 }}
-                    >
-                        {items.map((item, index) => (
-                            <Stack key={index} direction="row" sx={{ gap: 2 }}>
-                                {item.icon}
-                                <div>
-                                    <Typography gutterBottom sx={{ fontWeight: 'medium', fontSize: '3rem' }}>
-                                        {item.title}
-                                    </Typography>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                        {item.description}
-                                    </Typography>
-                                </div>
-                            </Stack>
-                        ))}
-                    </Grid>
-                    <Grid size={{ xs: 12, md: 6 }} spacing={4}><Box sx={{ padding: 6 }}>
-                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                            {/* <SitemarkIcon /> */}
-                        </Box>
-                        <Typography
-                            component="h1"
-                            variant="h4"
-                            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
-                        >
-                            Sign in
-                        </Typography>
-                        <Box>
-                            <Grid container sx={{ padding: 2 }}>
-                                {/* Left Column */}
-                                <Grid size={{ xs: 12, md: 6 }}>
-                                    <Box sx={{ padding: 2 }}>
-                                        <ThemeProvider theme={secondaryButtonTheme}>
-                                            <Button
-                                                type="submit"
-                                                fullWidth
-                                                variant="outlined"
-                                                onClick={() => alert('Sign in with Facebook')}
-                                                startIcon={<GoogleIcon />}
-                                            >
-                                                Sign in with Google
-                                            </Button>
-                                        </ThemeProvider>
-                                    </Box>
-                                </Grid>
-
-                                {/* Right Column */}
-                                <Grid size={{ xs: 12, md: 6 }}>
-                                    <Box sx={{ padding: 2 }}>
-                                        <ThemeProvider theme={secondaryButtonTheme}>
-                                            <Button
-                                                type="submit"
-                                                fullWidth
-                                                variant="outlined"
-                                                onClick={() => alert('Sign in with Facebook')}
-                                                startIcon={<FacebookIcon />}
-                                            >
-                                                Sign in with Facebook
-                                            </Button>
-                                        </ThemeProvider>
-                                    </Box>
-                                </Grid>
-                            </Grid>
-                        </Box><Divider>or</Divider>
-                        <Box
-                            component="form"
-                            onSubmit={handleSubmit}
-                            noValidate
-                            sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 2 }}
-                        >
-                            <FormControl>
-                                <FormLabel htmlFor="email">Email</FormLabel>
-                                <ThemeProvider theme={textTheme}>
-                                    <TextField
-                                        error={emailError}
-                                        helperText={emailErrorMessage}
-                                        id="email"
-                                        type="email"
-                                        name="email"
-                                        placeholder="your@email.com"
-                                        autoComplete="email"
-                                        autoFocus
-                                        required
-                                        fullWidth
-                                        variant="outlined"
-                                        color={emailError ? 'error' : 'primary'}
-                                        sx={{ ariaLabel: 'email' }}
-                                    /></ThemeProvider>
-                            </FormControl>
-                            <FormControl>
-                                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <FormLabel htmlFor="password">Password</FormLabel>
-                                </Box>
-                                <ThemeProvider theme={textTheme}>
-                                    <TextField
-                                        error={passwordError}
-                                        helperText={passwordErrorMessage}
-                                        name="password"
-                                        placeholder="••••••"
-                                        type="password"
-                                        id="password"
-                                        autoComplete="current-password"
-                                        autoFocus
-                                        required
-                                        fullWidth
-                                        variant="outlined"
-                                        color={passwordError ? 'error' : 'primary'}
-                                    />
-                                </ThemeProvider>
-                            </FormControl>
-                            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                <FormControlLabel
-                                    control={<Checkbox value="remember" color="primary" />}
-                                    label="Remember me"
-                                />
-                                <Link
-                                    component="button"
-                                    onClick={handleClickOpen}
-                                    variant="body2"
-                                    sx={{ alignSelf: 'baseline' }}
-                                >
-                                    Forgot your password?
-                                </Link>
-                            </Box>
-                            <ThemeProvider theme={primaryButtonTheme}>
-                                <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>Sign in</Button>
-                            </ThemeProvider>
-                            <ForgotPassword open={open} handleClose={handleClose} />
-                            <Typography sx={{ textAlign: 'center' }}>
-                                Don&apos;t have an account?{' '}
-                                <span>
-                                    <Link
-                                        href="/material-ui/getting-started/templates/sign-in/"
-                                        variant="body2"
-                                        sx={{ alignSelf: 'center' }}
-                                    >
-                                        Sign up
-                                    </Link>
-                                </span>
-                            </Typography>
-                        </Box>
-                    </Box>
-
-                    </Grid>
+            <Grid container spacing={4} sx={{
+                justifyContent: 'center',
+                p: 5,
+                m: 'auto',
+            }}>
+                <Grid spacing={4} size={{ xs: 12, md: 6 }} className="bg-img"
+                    sx={{ flexDirection: 'column', alignSelf: 'auto', gap: 4, color: '#fff', height: '100vh', padding: 6 }}
+                >
+                    {items.map((item, index) => (
+                        <Stack key={index} direction="row" sx={{ gap: 2 }}>
+                            {item.icon}
+                            <div>
+                                <Typography gutterBottom sx={{ fontWeight: 'medium', fontSize: '3rem' }}>
+                                    {item.title}
+                                </Typography>
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    {item.description}
+                                </Typography>
+                            </div>
+                        </Stack>
+                    ))}
                 </Grid>
-            </Stack>
+                <Grid size={{ xs: 12, md: 6 }} spacing={4}><Box sx={{ padding: 6 }}>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        {/* <SitemarkIcon /> */}
+                    </Box>
+                    <Typography
+                        component="h1"
+                        variant="h3"
+                        sx={{ width: '100%', fontSize: '2rem', fontWeight: '700' }}
+                    >
+                        Sign in
+                    </Typography>
+                    <Box>
+                        <Grid container sx={{ padding: 2 }}>
+                            {/* Left Column */}
+                            <Grid size={{ xs: 12, md: 6 }}>
+                                <Box sx={{ padding: 2 }}>
+                                    <ThemeProvider theme={secondaryButtonTheme}>
+                                        <Button
+                                            type="submit"
+                                            fullWidth
+                                            variant="outlined"
+                                            onClick={() => alert('Sign in with Apple')}
+                                            startIcon={<AppleIcon />}
+                                        >
+                                            Sign in with Apple
+                                        </Button>
+                                    </ThemeProvider>
+                                </Box>
+                            </Grid>
+
+                            {/* Right Column */}
+                            <Grid size={{ xs: 12, md: 6 }}>
+                                <Box sx={{ padding: 2 }}>
+                                    <ThemeProvider theme={secondaryButtonTheme}>
+                                        <Button
+                                            type="submit"
+                                            fullWidth
+                                            variant="outlined"
+                                            onClick={() => alert('Sign in with Google')}
+                                            startIcon={<GoogleIcon />}
+                                        >
+                                            Sign in with Google
+                                        </Button>
+                                    </ThemeProvider>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Box><Divider>or</Divider>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 2 }}
+                    >
+                        <FormControl>
+                            <FormLabel htmlFor="email">Email</FormLabel>
+                            <TextField
+                                error={emailError}
+                                helperText={emailErrorMessage}
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="your@email.com"
+                                autoComplete="email"
+                                autoFocus
+                                required
+                                fullWidth
+                                variant="outlined"
+                                color={emailError ? 'error' : 'primary'}
+                                sx={{
+                                    ariaLabel: 'email', '& .MuiOutlinedInput-root': {
+                                        borderRadius: '30px', // Apply border-radius here
+                                    },
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <FormLabel htmlFor="password">Password</FormLabel>
+                            </Box>
+                            <TextField
+                                error={passwordError}
+                                helperText={passwordErrorMessage}
+                                name="password"
+                                placeholder="••••••"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                autoFocus
+                                required
+                                fullWidth
+                                variant="outlined"
+                                sx={{
+                                    '& .MuiOutlinedInput-root': {
+                                        borderRadius: '30px', // Apply border-radius here
+                                    },
+                                }}
+                                color={passwordError ? 'error' : 'primary'}
+                            />
+                        </FormControl>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Link
+                                component="button"
+                                onClick={handleClickOpen}
+                                variant="body2"
+                                sx={{ alignSelf: 'baseline' }}
+                            >
+                                Forgot your password?
+                            </Link>
+                        </Box>
+                        <ThemeProvider theme={primaryButtonTheme}>
+                            <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>Sign in</Button>
+                        </ThemeProvider>
+                        <ForgotPassword open={open} handleClose={handleClose} />
+                        <Typography sx={{ textAlign: 'left' }}>
+                            Don&apos;t have an account?{' '}
+                            <span>
+                                <Link
+                                    href="/material-ui/getting-started/templates/sign-in/"
+                                    variant="body2"
+                                    sx={{ alignSelf: 'center' }}
+                                >
+                                    Sign up
+                                </Link>
+                            </span>
+                        </Typography>
+                    </Box>
+                </Box>
+
+                </Grid>
+            </Grid>
         </>
 
 
